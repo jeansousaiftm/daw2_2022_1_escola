a0b1e894e492e1116e6daede17e22cda260fd2ec
Jean <jeansousa@iftm.edu.br>
2022-04-12 20:10:59 -0300

    Aluno e Join

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -5,14 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Aluno;
 use App\Models\Curso;
+use Illuminate\Support\Facades\DB;
 
 class AlunoController extends Controller
 {
-
+	
+	public function listaAlunos() {
+		return DB::table("aluno AS a")
+					->join("curso AS c", "a.curso_id", "=", "c.id")
+					->select("a.*", "c.nome AS nome_curso")
+					->get();
+	}
+	
     public function index()
     {
         $aluno = new Aluno();
-		$alunos = Aluno::All();
+		$alunos = $this->listaAlunos();
 		$cursos = Curso::All();
 		return view("aluno.index", [
 			"aluno" => $aluno,
@@ -21,69 +29,41 @@ class AlunoController extends Controller
 		]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        if ($request->get("id") != "") {
+			$aluno = Aluno::Find($request->get("id"));
+		} else {
+			$aluno = new Aluno();
+		}
+		
+		$aluno->nome = $request->get("nome");
+		$aluno->email = $request->get("email");
+		$aluno->curso_id = $request->get("curso_id");
+		$aluno->save();
+		
+		$request->session()->flash("status", "salvo");
+		
+		return redirect("/aluno");
+		
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        //
+        $aluno = Aluno::Find($id);
+		$alunos = $this->listaAlunos();
+		$cursos = Curso::All();
+		return view("aluno.index", [
+			"aluno" => $aluno,
+			"alunos" => $alunos,
+			"cursos" => $cursos
+		]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function destroy($id, Request $request)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        Aluno::Destroy($id);
+		$request->session()->flash("status", "excluido");
+		return redirect("/aluno");
     }
 }
---
+++ resources/views/aluno/index.blade.php
@@ -0,0 +1,105 @@
+@extends("templates.main")
+
+@section("titulo", "Cadastro de Alunos")
+
+@section("formulario")
+	<br/>
+	<h1>Cadastro de Alunos</h1>
+	<form method="POST" action="/aluno" class="row">
+		<div class="form-group col-6">
+			<label for="nome">Nome:</label>
+			<input type="text" id="nome" name="nome" class="form-control" value="{{ $aluno->nome }}" required />
+		</div>
+		<div class="form-group col-6">
+			<label for="email">E-mail:</label>
+			<input type="mail" id="email" name="email" class="form-control" value="{{ $aluno->email }}" required />
+		</div>
+		<div class="form-group col-6">
+			<label for="curso_id">Curso:</label>
+			<select id="curso_id" name="curso_id" class="form-control" required>
+				<option value=""></option>
+				@foreach ($cursos as $curso) 
+					<option value="{{ $curso->id }}" @if ($curso->id == $aluno->curso_id) selected @endif>{{ $curso->nome }}</option>
+				@endforeach
+			</select>
+		</div>
+		<div class="form-group col-4">
+			<label for="foto">Foto:</label>
+			<input type="file" id="foto" name="foto" class="form-control" />
+		</div>
+		<div class="form-group col-2">
+			@csrf
+			<input type="hidden" id="id" name="id" value="{{ $aluno->id }}" />
+			<a href="/aluno" class="btn btn-sm btn-primary" style="margin-top: 29px;">
+				<i class="bi bi-plus-square"></i> Novo
+			</a>
+			<button type="submit" class="btn btn-sm btn-success" style="margin-top: 29px;">
+				<i class="bi bi-save"></i> Salvar
+			</button>
+		</div>
+	</form>
+@endsection
+
+@section("tabela")
+	<br/>
+	<h1>Lista de Alunos</h1>
+	<table class="table table-striped">
+		<colgroup>
+			<col width="300">
+			<col width="300">
+			<col width="300">
+			<col width="100">
+			<col width="100">
+		</colgroup>
+		<thead>
+			<tr>
+				<th>Nome</th>
+				<th>Curso</th>
+				<th>Foto</th>
+				<th>Editar</th>
+				<th>Excluir</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach ($alunos as $aluno)
+				<tr>
+					<td>{{ $aluno->nome }}</td>
+					<td>{{ $aluno->nome_curso }}</td>
+					<td></td>
+					<td>
+						<a href="/aluno/{{ $aluno->id }}/edit" class="btn btn-warning">
+							<i class="bi bi-pencil-square"></i> Editar
+						</a>
+					</td>
+					<td>
+						<form method="POST" action="/aluno/{{ $aluno->id }}">
+							@csrf
+							<input type="hidden" name="_method" value="DELETE" />
+							<button type="button" class="btn btn-danger" onclick="excluir(this);">
+								<i class="bi bi-trash"></i> Excluir
+							</button>
+						</form>
+					</td>
+				</tr>
+			@endforeach
+		</tbody>
+	</table>
+@endsection
+
+<script>
+	function excluir(btn) {
+		Swal.fire({
+			"title": "Deseja realmente excluir?",
+			"icon": "warning",
+			"showCancelButton": true,
+			"cancelButtonText": "Cancelar",
+			"confirmButtonText": "Confirmar",
+			"confirmButtonColor": "#3085d6",
+			"cancelButtonColor": "#d33"
+		}).then(function(result) {
+			if (result.isConfirmed) {
+				$(btn).parents("form").submit();
+			}
+		});
+	}
+</script>
